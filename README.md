6d5dd0cceabb1485fba6e2e1edf01ac3c45374ee
Kohki YAMAGIWA <tamanobi@gmail.com>
2015-07-21 03:46:53 +0900

    fix comment & write README.md

---
+++ README.md
@@ -0,0 +1,16 @@
+### files ###
+dbutil.py: database(plain tsv) utilities
+lookaround.py: creating database
+descriptor.py: extracting feature(FAST/ORB)
+gaze.py: searching similarity images interface
+
+### usage1:database ###
+  python lookaround.py [directory path]
+
+directory path: directory's path contains only images(excluding gif)
+
+### usage2:searching ###
+  python gaze.py [filename] [num_sim]
+
+filename: query file name
+num_sim: number of displaying similarity images(default=3)
--- gaze.py
+++ gaze.py
@@ -4,30 +4,41 @@ import dbutil, descriptor
 import sys
 
 if __name__ == '__main__':
-  import matplotlib.pyplot as plt
-  names, kps = dbutil.LoadDB()
-  flann = dbutil.IndexByFlann(kps)
-  test = '../ccv_test/data/image{0:06d}.png'
-  tar = int(sys.argv[1])
-  img = cv2.imread(test.format(tar))
-  voting = dbutil.QuerySearch(img, flann, len(names))
-  kpslen = np.array([float(len(kp)) for kp in kps], dtype=np.float32)
-  voting2 = voting.astype(np.float32) / kpslen
-  rank = []
-  for v,i in zip(voting.tolist(), [x for x in range(len(voting))]):
-    rank.append([v,i])
-  rank_sorted = sorted(rank, reverse=True)
-  #plt.bar([x for x in range(len(voting2))],voting2)
-  #plt.show()
-  #idx = np.argmax(voting2)
-  n_sim = 3
-  idx = []
-  for i in range(n_sim):
-    #print rank_sorted[i][1]
-    idx.append(rank_sorted[i][1])
-  #print 'id:{0}, val:{1}, {2}, ratio{3}'.format(idx, np.max(voting2), len(kps[idx]), voting2[idx]*1.0/len(kps[idx]))
-  for i,j in enumerate(idx):
-    imgs = cv2.imread(test.format(j+1))
-    cv2.imshow('rank{0:02d},{1:0.2e}'.format(i,rank_sorted[i][0]), imgs)
-  cv2.imshow('query', img)
-  cv2.waitKey(0)
+  argvs = sys.argv
+  argc = len(argvs)
+  if argc >= 3:
+    num_similar = int(sys.argv[2])
+  else:
+    num_similar = 3
+  if argc >= 2:
+    # Loading Query Image
+    query_file = sys.argv[1]
+    img = cv2.imread(query_file)
+    # Loading from database
+    names, keypoints = dbutil.LoadDB()
+    num_keypoints = np.array([float(len(kp)) for kp in keypoints], dtype=np.float32)
+    # Indexing keypoints by flann(k-NN/kd-tree)
+    flann = dbutil.IndexByFlann(keypoints)
+    # Search simirality images from database 
+    voting = dbutil.QuerySearch(img, flann, len(names))
+    match_ratio = voting.astype(np.float32) / num_keypoints
+    # Ranking based on match ratio
+    rank = []
+    for v,i in zip(match_ratio.tolist(), [x for x in range(len(voting))]):
+      rank.append([v,i])
+    rank_sorted = sorted(rank, reverse=True)
+    idx = []
+    for i in range(num_similar):
+      idx.append(rank_sorted[i][1])
+    # Display similarity images
+    for i,j in enumerate(idx):
+      imgs = cv2.imread(names[j])
+      print 'rank:{0:02d}\tscore:{1:0.2e}\t{2}'.format(i+1,rank_sorted[i][0], names[j])
+      cv2.imshow('rank{0:02d},{1:0.2e}'.format(i+1,rank_sorted[i][0]), imgs)
+    cv2.imshow('query', img)
+    cv2.waitKey(0)
+  else:
+    usage = '''
+      usage: program [query filename] [num. of similarity images(opt/default=3)]
+    '''
+    print usage
